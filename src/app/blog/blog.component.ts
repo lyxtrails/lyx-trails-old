a526804ec4db70a1b7da1322fe013b48fcd78eed
lyxtrails <lyxtrails@gmail.com>
2021-03-28 20:23:20 -0400

    Add editMode in blog

--- src/app/blog/blog.component.ts
+++ src/app/blog/blog.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
+import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs';
+import { DatePipe } from '@angular/common';
 
 @Component({
   template: `
     <ul style="list-style-type: none;" id="blogList">
       <li *ngFor="let blog of blogs | async; let i = index" >
+        <a *ngIf="editMode" style="color:#e05122; padding-right:10px; cursor:pointer;"
+           (click)="deleteBlog(blog['docID'])">Delete</a>
         <a style="cursor:pointer;" data-toggle="collapse" [attr.data-target]="'#blog'+i">
           {{blog['date']}} {{blog['title']}}
         </a>
@@ -15,12 +19,50 @@ import { Observable } from 'rxjs';
         </div>
       </li>
     </ul>
+    <div *ngIf="editMode">
+      <mat-form-field style="width: 100%;">
+        <mat-label style="color:white">Title</mat-label>
+        <input #blogTitle matInput>
+      </mat-form-field>
+      <mat-form-field style="width: 100%;">
+        <mat-label style="color:white">Content</mat-label>
+        <textarea #blogContent matInput></textarea>
+      </mat-form-field>
+      <mat-form-field style="width: 100%;">
+        <mat-label style="color:white">Extra HTML</mat-label>
+        <textarea #blogExHtml matInput></textarea>
+      </mat-form-field>
+      <button (click)="addBlog(blogTitle.value, blogContent.value, blogExHtml.value)"
+              class="btn btn-secondary">
+        Submit
+      </button>
+    </div>
   `
 })
 
 export class BlogComponent {
+  db: AngularFirestore;
   blogs: Observable<any[]>;
-  constructor(private db: AngularFirestore) {
-    this.blogs = db.collection('Blogs').valueChanges();
+  editMode: boolean;
+  constructor(private af: AngularFirestore, route: ActivatedRoute) {
+    this.db = af;
+    this.blogs = this.db.collection('Blogs').valueChanges({idField: 'docID'})
+    route.queryParams.subscribe(params => {
+        this.editMode = params['editMode'];
+    });
+  }
+
+  addBlog(title: string, content: string, exHtml: string) {
+    const datepipe: DatePipe = new DatePipe('en-US')
+    let date = datepipe.transform(new Date(), 'yyyy.MM.dd')
+    this.db.collection("Blogs").add({
+        title: title,
+        date: date,
+        content: content,
+        html: exHtml
+    })
+  }
+  deleteBlog(docID: string) {
+    this.db.collection("Blogs").doc(docID).delete()
   }
 }
--- src/app/blog/blog.module.ts
+++ src/app/blog/blog.module.ts
@@ -4,16 +4,23 @@ import { BlogComponent } from './blog.component';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { PipeModule } from '../pipe.module'
 import { BrowserModule } from '@angular/platform-browser'
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { DatePipe } from '@angular/common';
 
 @NgModule({
   imports: [
     AngularFirestoreModule,
     PipeModule,
     BrowserModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   declarations: [
     BlogComponent,
   ],
-  providers: []
+  providers: [
+    DatePipe,
+  ]
 })
 export class BlogModule {}
